cf17f0f9e105a380978e1f1bc1f0aa37f42cdfb6
kiplongu <rodgersbiwott2016@gmail.com>
2024-07-09 09:42:56 +0300

    Implemented signup feature

--- social_book/core/models.py
+++ social_book/core/models.py
@@ -8,7 +8,7 @@ class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
-    profileimg = models.ImageField(upload_to='profile_images', default='black_profile_img')
+    profileimg = models.ImageField(upload_to='profile_images', default='black_profile_img.jpeg')
     location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
