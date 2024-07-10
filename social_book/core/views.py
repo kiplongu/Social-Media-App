eff118fd50be84d09421b561da8323358c4e3c0e
kiplongu <rodgersbiwott2016@gmail.com>
2024-07-10 09:09:46 +0300

    Implemented Upload pic form feature

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -3,16 +3,30 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from .models import Profile
+from .models import Profile, Post
 
 # Create your views here.
 @login_required(login_url="signin")
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request, 'index.html', {'user_profile':user_profile})
 
 @login_required(login_url="signin")
 def upload(request):
-    return HttpResponse("<h1> Upload View </h1>")
+
+    if request.method == "POST":
+        user = request.user.username
+        image = request.FILES.get("image_upload")
+        caption = request.POST["caption"]
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+
+        return redirect("/")
+    
+    else:
+        return redirect("/")
 
 
 @login_required(login_url="signin")
